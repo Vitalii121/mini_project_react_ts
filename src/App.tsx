43dec78001ab6bc933bd5b70ba7c3abf254362a6
GrayHead <ss.zhuravlov@gmail.com>
2023-11-05 20:35:55 +0200

    base

--- src/App.tsx
+++ src/App.tsx
@@ -7,3 +7,6 @@ const App = () => {
 };
 
 export {App};
+
+
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YTE4NjhjNjFkZmE3OWNlMDIwYmIyZjhhNjE4NTRkZSIsInN1YiI6IjY1NDdkOTZkZDhjYzRhMDEwMDViNDhhOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EjGbe8NrWhfqIw9Obh1cJLVy_u8iDi_ao9XYVLkv3dE
